36432755119942f0e34007b564f1176b1524d52b
Selena Sean <selena.sean.1994@gmail.com>
2023-03-08 18:11:23 +0100

    creation layout + activity detailNeighbour

---
+++ app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/DetailNeighbourActivity.java
@@ -0,0 +1,21 @@
+package com.openclassrooms.entrevoisins.ui.neighbour_list;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.openclassrooms.entrevoisins.databinding.ActivityDetailNeighbourBinding;
+
+public class DetailNeighbourActivity extends AppCompatActivity {
+
+    private ActivityDetailNeighbourBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityDetailNeighbourBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+    }
+}
